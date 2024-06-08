d8200af97b070b18e6bd4951ca876d5e4a6520f9
webdesign2201 <vibhushasontakke@gmail.com>
2024-06-08 23:23:36 +0530

    currTab

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,16 @@ import Sidebar from "./Components/Sidebar";
 import Footer from "./Components/Footer";
 import CreatePost from "./Components/CreatePost";
 import PostList from "./Components/PostList";
+import { useState } from "react";
 
 function App() {
+  const [currTab, setCurrTab] = useState("Home");
   return (
     <div className="Container">
-      <Sidebar></Sidebar>
+      <Sidebar currTab={currTab} setCurrTab={setCurrTab}></Sidebar>
       <div className="Main">
         <Header></Header>
-        <CreatePost></CreatePost>
-        <PostList></PostList>
+        {currTab === "Home" ? <PostList></PostList> : <CreatePost></CreatePost>}
         <Footer></Footer>
       </div>
     </div>
--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,32 +1,49 @@
-function Sidebar() {
+function Sidebar({ currTab, setCurrTab }) {
   return (
     <>
       <div
-        class="d-flex flex-column flex-shrink-0 p-3 text-bg-dark bar"
+        className="d-flex flex-column flex-shrink-0 p-3 text-bg-dark bar"
         style={{ width: "280px" }}
       >
         <a
           href="/"
-          class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none"
+          className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none"
         >
-          <svg class="bi pe-none me-2" width="40" height="32">
+          <svg className="bi pe-none me-2" width="40" height="32">
             <use xlink:href="#bootstrap"></use>
           </svg>
-          <span class="fs-4">AppName</span>
+          <span className="fs-4">AppName</span>
         </a>
         <hr />
-        <ul class="nav nav-pills flex-column mb-auto">
-          <li class="nav-item">
-            <a href="#" class="nav-link active" aria-current="page">
-              <svg class="bi pe-none me-2" width="16" height="16">
+        <ul className="nav nav-pills flex-column mb-auto">
+          <li className="nav-item">
+            <a
+              onClick={() => {
+                setCurrTab("Home");
+              }}
+              href="#"
+              className={`nav-link text-white ${
+                currTab === "Home" && "active"
+              }`}
+              aria-current="page"
+            >
+              <svg className="bi pe-none me-2" width="16" height="16">
                 <use xlink:href="#home"></use>
               </svg>
               Home
             </a>
           </li>
           <li>
-            <a href="#" class="nav-link text-white">
-              <svg class="bi pe-none me-2" width="16" height="16">
+            <a
+              onClick={() => {
+                setCurrTab("Create");
+              }}
+              href="#"
+              className={`nav-link text-white ${
+                currTab === "Create" && "active"
+              }`}
+            >
+              <svg className="bi pe-none me-2" width="16" height="16">
                 <use xlinkHref="#speedometer2" />
               </svg>
               Create
@@ -34,10 +51,10 @@ function Sidebar() {
           </li>
         </ul>
         <hr />
-        <div class="dropdown">
+        <div className="dropdown">
           <a
             href="#"
-            class="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
+            className="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
             data-bs-toggle="dropdown"
             aria-expanded="false"
           >
@@ -46,31 +63,31 @@ function Sidebar() {
               alt=""
               width="32"
               height="32"
-              class="rounded-circle me-2"
+              className="rounded-circle me-2"
             />
             <strong>mdo</strong>
           </a>
-          <ul class="dropdown-menu dropdown-menu-dark text-small shadow">
+          <ul className="dropdown-menu dropdown-menu-dark text-small shadow">
             <li>
-              <a class="dropdown-item" href="#">
+              <a className="dropdown-item" href="#">
                 New project...
               </a>
             </li>
             <li>
-              <a class="dropdown-item" href="#">
+              <a className="dropdown-item" href="#">
                 Settings
               </a>
             </li>
             <li>
-              <a class="dropdown-item" href="#">
+              <a className="dropdown-item" href="#">
                 Profile
               </a>
             </li>
             <li>
-              <hr class="dropdown-divider" />
+              <hr className="dropdown-divider" />
             </li>
             <li>
-              <a class="dropdown-item" href="#">
+              <a className="dropdown-item" href="#">
                 Sign out
               </a>
             </li>
