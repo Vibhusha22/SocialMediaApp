64ba324677947721a2dce95b5b67004e8337e3fb
webdesign2201 <vibhushasontakke@gmail.com>
2024-07-30 23:02:27 +0530

    header changes

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -19,13 +19,13 @@ function Header() {
               </svg>
             </a>
 
-            {/* <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+            <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
               <li>
                 <a href="#" className="nav-link px-2 text-secondary">
                   Home
                 </a>
               </li>
-              <li>
+              {/* <li>
                 <a href="#" className="nav-link px-2 text-white">
                   Features
                 </a>
@@ -34,7 +34,7 @@ function Header() {
                 <a href="#" className="nav-link px-2 text-white">
                   Pricing
                 </a>
-              </li>
+              </li> */}
               <li>
                 <a href="#" className="nav-link px-2 text-white">
                   FAQs
@@ -47,7 +47,7 @@ function Header() {
               </li>
             </ul>
 
-            <form
+            {/* <form
               className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3"
               role="search"
             >
@@ -59,14 +59,14 @@ function Header() {
               />
             </form> */}
 
-            {/* <div className="text-end">
+            <div className="text-end">
               <button type="button" className="btn btn-outline-light me-2">
                 Login
               </button>
               <button type="button" className="btn btn-warning">
                 Sign-up
               </button>
-            </div> */}
+            </div>
           </div>
         </div>
       </header>
